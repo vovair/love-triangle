cdc2681c93f03edad153a2567c0ee478dbaae468
vovair <vovair@gmail.com>
2018-09-17 22:55:10 +0300

    Added finding love triangles.

--- src/index.js
+++ src/index.js
@@ -3,5 +3,18 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  if (preferences.length == 0) return 0;
+  const isValid = index => {
+    return index > 0 && index <= preferences.length;
+  };
+  const isTriangle = first => {
+    const second = preferences[first - 1];
+    if (!isValid(second) || second == first) return false;
+    const third = preferences[second - 1];
+    if (!isValid(third) || third == second) return false;
+    return preferences[third - 1] == first;
+  };
+  let count = 0;
+  for (let i = 1; i <= preferences.length; i++) if (isTriangle(i)) count++;
+  return count / 3;
 };
